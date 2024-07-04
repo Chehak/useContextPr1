42a306dc2dc0ebb5777c90715cbf25fa88c0c025
Chehak Gupta <gchehak18@gmail.com>
2024-07-04 13:10:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-
-import './App.css';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import UserContextProvider from './contexts/UserContextProvider';
+import "./App.css";
+import UserContextProvider from "./contexts/UserContextProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Users from "./components/Users";
+import { ThemeContextprovider } from "./contexts/ThemeContext";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const darkMode = () => {
+    setThemeMode("dark");
+  };
+  const lightMode = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light", "dark");
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
-   <UserContextProvider>
-    <Login />
-    <Profile />
-   </UserContextProvider>
+    <>
+      <UserContextProvider>
+        <Login />
+        <Profile />
+        <Users />
+      </UserContextProvider>
+
+      <ThemeContextprovider value={{ themeMode, darkMode, lightMode }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeContextprovider>
+    </>
   );
 }
 
